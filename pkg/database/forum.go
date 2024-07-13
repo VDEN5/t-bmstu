98e122b69b6d9cfbf857fe05f251de22e2436a45
VDEN5 <156585231+VDEN5@users.noreply.github.com>
2024-07-13 13:21:47 +0300

    add get msg

--- pkg/database/forum.go
+++ pkg/database/forum.go
@@ -28,3 +28,28 @@ func CreateMessage(msg Message) (int, error) {
 
 	return contestID, nil
 }
+
+func MessageExist(msgID string) (bool, Message, error) {
+	conn, err := pgx.Connect(context.Background(), DbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Выполнение запроса на проверку наличия сообщения в таблице
+	var msg Message
+	err = conn.QueryRow(context.Background(), "SELECT * FROM forumtable WHERE id = $1", msgID).Scan(
+		&msg.ID,
+		&msg.ForumTheme,
+		&msg.ForumTask,
+		&msg.ForumUser,
+	)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return false, Message{}, nil
+		}
+		return false, Message{}, fmt.Errorf("ошибка при выполнении запроса: %w", err)
+	}
+
+	return true, msg, nil
+}
