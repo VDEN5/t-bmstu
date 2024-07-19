658acfc6dba788a8f3166b5862866d61bffae779
VDEN5 <156585231+VDEN5@users.noreply.github.com>
2024-07-19 19:43:43 +0300

    add getpoints

--- pkg/database/contest.go
+++ pkg/database/contest.go
@@ -2,9 +2,75 @@ package database
 
 import (
 	"context"
-	"github.com/jackc/pgx/v4"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/jackc/pgx/v4"
 )
+
+func getpointstimus(id int) int {
+	reqURL := "https://timus.online/problem.aspx?space=1&num=" + strconv.Itoa(id)
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	str := string(body)
+	ind := strings.Index(str, "problem_links")
+	str = str[ind+33:]
+	ind = strings.Index(str, "</SPAN>")
+	str = str[:ind]
+	res, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("this is not number %s", err)
+		return -1
+	}
+	return res
+}
+
+func getpointsacmp(id int) int {
+	reqURL := "https://acmp.ru/index.asp?main=task&id_task=" + strconv.Itoa(id)
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	str := string(body)
+	ind := strings.Index(str, "</h1>")
+	str = str[ind:]
+	ind = strings.Index(str, "</i>")
+	str = str[:ind]
+	ind = strings.Index(str, "%")
+	str = str[ind-8 : ind]
+	ind = strings.Index(str, ": ")
+	str = str[ind+2:]
+	res, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Printf("this is not number %s", err)
+		return -1
+	}
+	return res
+}
 
 func GetContestInfoById(id int) (Contest, error) {
 	conn, err := pgx.Connect(context.Background(), DbURL)
